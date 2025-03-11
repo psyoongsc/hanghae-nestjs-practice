bb0316baaf507d92d5102bf97c672512eec76455
psyoongsc <psyoongsc@naver.com>
2025-03-12 08:03:25 +0900

    Full CRUD for comment feature

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,7 +1,31 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Comment } from './interfaces/comment.interface'
 import { CommentService } from './comment.service';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { UpdateCommentDto } from './dto/update-comment.dto';
+import { RemoveCommentDto } from './dto/remove-comment.dto';
 
 @Controller('comment')
 export class CommentController {
     constructor(private readonly commentService: CommentService) {}
+
+    @Get(':postingId')
+    getAllComments(@Param('postingId') postingId: string): Promise<Comment[]> {
+        return this.commentService.getAllComments(postingId);
+    }
+
+    @Post('create/:postingId')
+    createComment(@Param('postingId') postingId: string, @Body() createCommentDto: CreateCommentDto): Promise<Comment> {
+        return this.commentService.createComment(postingId, createCommentDto);
+    }
+
+    @Post('update/:id')
+    updateComment(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto): Promise<Comment> {
+        return this.commentService.updateComment(id, updateCommentDto);
+    }
+
+    @Post('remove/:id')
+    removeComment(@Param('id') id: string, @Body() removeCommentDto: RemoveCommentDto): Promise<Comment> {
+        return this.commentService.removeComment(id, removeCommentDto);
+    }
 }
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -7,7 +7,25 @@ import { Comment } from './interfaces/comment.interface'
 export class CommentService {
     constructor(@InjectModel('Comment') private readonly commentModel: Model<Comment>) {}
 
-    getAllComments(id: string): Promise<Comment[]> {
-        return this.commentModel.find({ postingId: id }).exec();
+    getAllComments(postingId: string): Promise<Comment[]> {
+        return this.commentModel.find({ postingId: postingId }).exec();
+    }
+
+    createComment(postingId: string, comment: Comment): Promise<Comment> {
+        comment.postingId = postingId;
+        comment.createdAt = new Date();
+
+        const createdComment = new this.commentModel(comment);
+        return createdComment.save();
+    }
+
+    updateComment(id: string, comment: Comment): Promise<Comment> {
+        return this.commentModel.findOneAndUpdate({ id: id, author: comment.author }, {
+            comment: comment.context
+        }).exec().then();
+    }
+
+    removeComment(id: string, comment: Comment): Promise<Comment> {
+        return this.commentModel.deleteOne({ id: id, author: comment.author }).exec().then();
     }
 }
