5723841de4d06c5fca2c7036d819fe8be982852e
psyoongsc <psyoongsc@naver.com>
2025-03-11 19:54:36 +0900

    comment CRUD - Display

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -7,4 +7,7 @@ import { Comment } from './interfaces/comment.interface'
 export class CommentService {
     constructor(@InjectModel('Comment') private readonly commentModel: Model<Comment>) {}
 
+    getAllComments(id: string): Promise<Comment[]> {
+        return this.commentModel.find({ postingId: id }).exec();
+    }
 }
