496882a8d8cb0608d3f559d4f61fe22c85aaa5ac
psyoongsc <psyoongsc@naver.com>
2025-03-11 16:46:26 +0900

    'id' props deleted, handled with _id

--- src/posting/schemas/posting.schema.ts
+++ src/posting/schemas/posting.schema.ts
@@ -1,7 +1,6 @@
 import { Schema } from 'mongoose';
 
 export const PostingSchema = new Schema({
-    id: { type: Number },
     title: { type: String },
     context: { type: String },
     postedDatetime: { type: Date },
