2768b4c92ee67718d786b8d331b7faacfa551568
psyoongsc <psyoongsc@naver.com>
2025-03-11 09:32:44 +0900

    comment feature added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostingModule } from './posting/posting.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [PostingModule],
+  imports: [PostingModule, CommentModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/comment/comment.controller.ts
@@ -0,0 +1,6 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+@Controller('comment')
+export class CommentController {
+    
+}
