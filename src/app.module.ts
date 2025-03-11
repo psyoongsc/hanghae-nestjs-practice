7944d66f3a1b994b93c4df25a42c4d21212fbeb9
psyoongsc <psyoongsc@naver.com>
2025-03-11 13:31:03 +0900

    mongo connection added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostingModule } from './posting/posting.module';
 import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [PostingModule, CommentModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/postings'),
+    PostingModule, 
+    CommentModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/posting/posting.module.ts
+++ src/posting/posting.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PostingService } from './posting.service';
 import { PostingController } from './posting.controller';
+import { PostingSchema } from './schemas/posting.schema';
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([{ name: 'Posting', schema: PostingSchema }]), // Register the model here
+    ],
     controllers: [PostingController],
     providers: [PostingService]
 })
