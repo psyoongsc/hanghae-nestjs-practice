57596e0a11b2598cf08f81f545021109fad17cf1
psyoongsc <psyoongsc@naver.com>
2025-03-11 14:42:09 +0900

    remove ArrayDB, added MongoDB for Posting API

--- src/posting/posting.controller.ts
+++ src/posting/posting.controller.ts
@@ -10,38 +10,27 @@ export class PostingController {
     constructor(private readonly postingService: PostingService) {}
 
     @Get('all')
-    getAllPostings(): Posting[] {
+    getAllPostings(): Promise<Posting[]> {
         return this.postingService.getAllPostings();
     }
 
-    // mongo
-    @Get('/all-mongo')
-    getAllPostingsMongo(): Promise<Posting[]> {
-        return this.postingService.getAllPostingsMongo();
-    }
-
     @Get(':id')
-    getPostingById(@Param('id') id: number): Posting | undefined {
+    getPostingById(@Param('id') id: number): Promise<Posting[]> {
         return this.postingService.getPostingById(id);
     }
 
     @Post('create')
-    createPosting(@Body() createPostingDto: CreatePostingDto): Posting {
+    createPosting(@Body() createPostingDto: CreatePostingDto): Promise<Posting> {
         return this.postingService.createPosting(createPostingDto);
     }
 
-    @Post('create-mongo')
-    createPostingMongo(@Body() createPostingDto: CreatePostingDto): Promise<Posting> {
-        return this.postingService.createPostingMongo(createPostingDto);
-    }
-
     @Post('update/:id')
-    updatePosting(@Param('id') id: number, @Body() updatePostingDto: UpdatePostingDto): Posting | undefined {
+    updatePosting(@Param('id') id: number, @Body() updatePostingDto: UpdatePostingDto): Promise<Posting> {
         return this.postingService.updatePosting(id, updatePostingDto);
     }
 
     @Post('remove/:id')
-    removePosting(@Param('id') id: number, @Body() removePostingDto: RemovePostingDto): Posting | undefined {
+    removePosting(@Param('id') id: number, @Body() removePostingDto: RemovePostingDto): Promise<Posting> {
         return this.postingService.removePosting(id, removePostingDto);
     }
     
--- src/posting/posting.service.ts
+++ src/posting/posting.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Posting } from './interfaces/posting.interface'
@@ -9,16 +9,7 @@ export class PostingService {
     private postings: Posting[] = [];
     private len: number = 0;
 
-    createPosting(posting: Posting): Posting {
-        posting.id = ++this.len;
-        posting.postedDatetime = new Date();
-
-        this.postings.push(posting);
-        return posting;
-    }
-
-    // mongo 
-    createPostingMongo(posting: Posting): Promise<Posting> {
+    createPosting(posting: Posting): Promise<Posting> {
         posting.id = ++this.len;
         posting.postedDatetime = new Date();
 
@@ -26,51 +17,24 @@ export class PostingService {
         return createdPosting.save();
     }
 
-    getAllPostings(): Posting[] {
-        return this.postings;
-    }
-
-    // mongo
-    getAllPostingsMongo(): Promise<Posting[]> {
+    getAllPostings(): Promise<Posting[]> {
         return this.postingModel.find().exec();
     }
 
-    getPostingById(id: number): Posting | undefined {
-        return this.postings.find((posting) => posting.id == id);
+    getPostingById(id: number): Promise<Posting[]> {
+        return this.postingModel.find({id: id}).exec();
     }
 
-    updatePosting(id:number, posting: Posting): Posting | undefined {
-        const edittingPosting = this.postings.find((item) => item.id == id);
+    updatePosting(id: number, posting: Posting): Promise<Posting> {
+        return this.postingModel.findOneAndUpdate({id: id, password: posting.password}, {
+            title: posting.title,
+            context: posting.context
+        }).exec().then()
 
-        if(edittingPosting === undefined) {
-            return undefined;
-        }
-
-        if(edittingPosting.password == posting.password) {
-            edittingPosting.title = posting.title;
-            edittingPosting.context = posting.context;
-
-            return edittingPosting;
-        }
-        else {
-            return undefined;
-        }
     }
 
-    removePosting(id: number, posting: Posting): Posting | undefined {
-        const removingPosting = this.postings.find((item) => item.id == id);
-
-        if(removingPosting === undefined) {
-            return undefined;
-        }
-
-        if(removingPosting.password == posting.password) {
-            this.postings = this.postings.filter((item) => item.id != id);
-
-            return removingPosting;
-        }
-        else {
-            return undefined;
-        }
+    removePosting(id: number, posting: Posting): Promise<Posting> {
+        return this.postingModel.findOneAndDelete({id: id, password: posting.password})
+        .exec().then();
     }
 }
