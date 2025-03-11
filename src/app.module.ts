12a1aa6b3791dfdbe5157458e06fffafa7630b95
psyoongsc <psyoongsc@naver.com>
2025-03-11 10:24:51 +0900

    inject mongo connection into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostingModule } from './posting/posting.module';
 import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [PostingModule, CommentModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/postings', { connectionName: 'postings'}),
+    MongooseModule.forRoot('mongodb://localhost/commets', { connectionName: 'comments' }),
+    PostingModule, CommentModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
