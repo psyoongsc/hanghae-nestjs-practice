5d707806fc081e3654e9d23c38e90cfe1aeac4f2
psyoongsc <psyoongsc@naver.com>
2025-03-11 11:13:27 +0900

    Revert "inject mongo connection into app module"
    This reverts commit 12a1aa6b3791dfdbe5157458e06fffafa7630b95.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostingModule } from './posting/posting.module';
 import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost/postings', { connectionName: 'postings'}),
-    MongooseModule.forRoot('mongodb://localhost/commets', { connectionName: 'comments' }),
-    PostingModule, CommentModule
-  ],
+  imports: [PostingModule, CommentModule],
   controllers: [AppController],
   providers: [AppService],
 })
